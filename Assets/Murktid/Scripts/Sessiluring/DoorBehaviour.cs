17e0170468aedf88ecc15f075ebf18e6e1dc3bca
Cia <cia_wretemark@hotmail.com>
2025-11-25 17:16:09 +0100

    grid cleaning and door opening stuff

--- Assets/Murktid/Scripts/Sessiluring/DoorBehaviour.cs
+++ Assets/Murktid/Scripts/Sessiluring/DoorBehaviour.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
 
-public class DoorBehaviour : MonoBehaviour
-{
+public class DoorBehaviour : MonoBehaviour {
+    public bool _isDoorOpen = false;
+    Vector3 _doorClosedPos;
+    Vector3 _doorOpenPos;
+    public float _doorSpeed = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    void Awake() {
+        _doorClosedPos = transform.position;
+        _doorOpenPos = new Vector3(transform.position.x, transform.position.y + 4f, transform.position.z);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if(_isDoorOpen) {
+            OpenDoor();
+        }
+        else if(!_isDoorOpen) {
+            ClosedDoor();
+        }
+    }
+
+    void OpenDoor() {
+        if(transform.position != _doorOpenPos) {
+            transform.position = Vector3.MoveTowards(transform.position, _doorOpenPos, _doorSpeed * Time.deltaTime);
+        }
+    }
+
+    void ClosedDoor() {
+        if(transform.position != _doorClosedPos) {
+            transform.position = Vector3.MoveTowards(transform.position, _doorClosedPos, _doorSpeed * Time.deltaTime);
+        }
     }
 }
--- Assets/Murktid/Scripts/Sessiluring/SwitchBehaviour.cs
+++ Assets/Murktid/Scripts/Sessiluring/SwitchBehaviour.cs
@@ -1,16 +1,79 @@
+using System.Collections;
 using UnityEngine;
 
 public class SwitchBehaviour : MonoBehaviour
 {
+    [SerializeField] DoorBehaviour _doorBehaviour;
+
+    [SerializeField] bool _isDoorOpenSwitch;
+    [SerializeField] bool _isDoorClosedSwitch;
+
+
+    float _switchSizeZ;
+    Vector3 _switchInPos;
+    Vector3 _switchOutPos;
+    float _switchSpeed =1f;
+    float _switchDelay = 0.2f;
+    bool _isPressingSwitch = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        _switchSizeZ = transform.localScale.z / 2;
+
+        _switchInPos = transform.position;
+        _switchOutPos = new Vector3(transform.position.x, transform.position.y - _switchSizeZ, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_isPressingSwitch) {
+            MoveSwitchIn();
+        }
+        else if(!_isPressingSwitch) {
+            MoveSwitchOut();
+        }
+    }
+
+    void MoveSwitchIn() {
+        if(transform.position != _switchInPos) {
+            transform.position = Vector3.MoveTowards(transform.position, _switchInPos, _switchSpeed * Time.deltaTime);
+        }
+    }
+
+    void MoveSwitchOut() {
+        if(transform.position != _switchOutPos) {
+            transform.position = Vector3.MoveTowards(transform.position, _switchOutPos, _switchSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+        {
+        if(collision.CompareTag("Player"))
+            {
+            _isPressingSwitch = !_isPressingSwitch;
+
+            if(_isDoorOpenSwitch && !_doorBehaviour._isDoorOpen)
+            {
+                _doorBehaviour._isDoorOpen = !_doorBehaviour._isDoorOpen;
+            }
+            else if(_isDoorClosedSwitch && _doorBehaviour._isDoorOpen) {
+                _doorBehaviour._isDoorOpen = !_doorBehaviour._isDoorOpen;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collision) {
+
+        if(collision.CompareTag("Player")) {
+
+            StartCoroutine(SwitchOutDelay(_switchDelay));
+        }
+
+        }
+    IEnumerator SwitchOutDelay(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        _isPressingSwitch = false;
     }
 }
+
