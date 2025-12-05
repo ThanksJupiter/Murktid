a23deec9834c5bda0057d39c8649f03b01197445
Cia <cia_wretemark@hotmail.com>
2025-12-05 09:44:18 +0100

    Nest Health

--- Assets/Murktid/Scripts/Sessiluring/NestTarget.cs
+++ Assets/Murktid/Scripts/Sessiluring/NestTarget.cs
@@ -4,10 +4,36 @@ namespace Murktid {
 
     public class NestTarget : MonoBehaviour, ITarget {
 
+        [SerializeField] private Transform healthBarPivot;
+       [SerializeField] private Vector3 healthBarOffset = new Vector3 (0f, 2f, 0f);
+
+        [SerializeField] private float maxHealth = 500f;
+        private float currentHealth;
+
         [SerializeField] private BreakableWall[] wallsToBreak;
+        
+
+        private void Awake() {
+            currentHealth = maxHealth;
+        }
+
+        private void UpdateHealthBar() {
+            if(healthBarPivot == null) return;
+
+            float normalized = currentHealth / maxHealth;
+            healthBarPivot.localScale = new Vector3(normalized, 1f, 1f);
+
+           if (Camera.main != null)
+                healthBarPivot.LookAt(Camera.main.transform);
+        }
 
         public void Hit(float damage) {
 
+            currentHealth -= damage;
+            UpdateHealthBar();
+
+            if(currentHealth > 0f)
+                return;
 
             //Debug.Log($"Hit: { gameObject.name } for { damage }");
             if(wallsToBreak != null) {
