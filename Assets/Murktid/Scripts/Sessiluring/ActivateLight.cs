e94f36c50a53141f513260a65168a8a6a701b61a
CiaCecilia <35657860+ThanksJupiter@users.noreply.github.com>
2026-01-22 14:39:24 +0100

    LIghts, storry beast, level flow

--- Assets/Murktid/Scripts/Sessiluring/ActivateLight.cs
+++ Assets/Murktid/Scripts/Sessiluring/ActivateLight.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class ActivateLight : MonoBehaviour
 {
     public Light light;
+    public ParticleSystem particleSystem;
 
     public void Activate() {
         Debug.Log("Light is Activated");
             light.enabled = true;
+            particleSystem.Play();
 
     }
 }
