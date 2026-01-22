583532e51ef47657319fbd3fa5b497d94789ec40
ThanksJupiter <35657860+ThanksJupiter@users.noreply.github.com>
2026-01-22 22:17:33 +0100

    flickering lights

--- Assets/Murktid/Scripts/Sessiluring/ActivateLight.cs
+++ Assets/Murktid/Scripts/Sessiluring/ActivateLight.cs
@@ -1,9 +1,15 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ActivateLight : MonoBehaviour
 {
     public Light light;
     public ParticleSystem particleSystem;
+
+    private float timer = 1f;
+    private float lerpSpeed = 1f;
+    private float intensityTarget = 30f;
 
     public void Activate() {
         //Debug.Log("Light is Activated");
@@ -12,5 +18,18 @@ public class ActivateLight : MonoBehaviour
                 particleSystem.Play();
             }
 
+    }
+
+    private void Update() {
+        if(light.enabled) {
+            light.intensity = Mathf.Lerp(light.intensity, intensityTarget, 1f - Mathf.Exp(-lerpSpeed * Time.deltaTime));
+            timer -= Time.deltaTime;
+
+            if(timer <= 0f) {
+                timer = Random.Range(.1f, .25f);
+                lerpSpeed = Random.Range(3f, 7f);
+                light.intensity = Random.Range(15f, 35f);
+            }
+        }
     }
 }
